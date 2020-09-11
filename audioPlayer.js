75d6c660b463d26ed63021df0880e5410e3772d3
Manish <manihs@gmail.com>
2020-09-11 11:21:25 +0545

    Update current time and progress bar

--- audioPlayer.js
+++ audioPlayer.js
@@ -1,7 +1,8 @@
-let fillBar = document.querySelector(".fill");
+let fillBar = document.querySelector(".seek .fill");
 const playPauseBtn = document.querySelector(".play-pause");
-let audios = ["song/1.mp3", "song/2.mp3", "song/3.mp3", "song/4.mp3", "song/5.mp3", "song/6.mp3"];
-const coverImg = ["img/1.jpg", "img/2.jpg", "img/3.jpg", "img/4.jpg", "img/5.jpg", "img/6.jpg", "img/7.jpg"];
+const currentTime = document.querySelector(".time");
+let audios = ["song/one.mp3", "song/1.mp3", "song/2.mp3", "song/3.mp3", "song/4.mp3", "song/5.mp3"];
+const coverImg = ["img/1.jpg", "img/2.jpg", "img/3.jpg", "img/4.jpg", "img/5.jpg", "img/6.jpg"];
 
 
 //
@@ -30,4 +31,38 @@ function togglePlayPause() {
       playPauseBtn.innerHTML = `<i class="fa fa-play"></i>`;
       playPauseBtn.style.paddingLeft = "33px";
    }
+}
+
+//
+//Increasing in time, increase progress bar
+audio.addEventListener("timeupdate", function () {
+   let position = audio.currentTime / audio.duration;
+   fillBar.style.width = position * 100 + "%";
+
+   //display currentTime and totalDuration
+   convertTime(Math.floor(audio.currentTime));
+})
+
+function convertTime(seconds) {
+   let min = Math.floor(seconds / 60);
+   let sec = seconds % 60;
+
+   //if single digit exists then add prefix on sec/min
+   min = min < 10 ? "0" + min : min;
+   sec = sec < 10 ? "0" + sec : sec;
+   currentTime.textContent = `${min}:${sec}`;
+
+   //display the total duration time
+   //required min and sec
+   totalDuration(Math.round(audio.duration));
+}
+
+function totalDuration(seconds) {
+   let min = Math.floor(seconds / 60);
+   let sec = seconds % 60;
+
+   //if single digit exists then add prefix on sec/min
+   min = min < 10 ? "0" + min : min;
+   sec = sec < 10 ? "0" + min : sec;
+   currentTime.textContent += ` & ${min}:${sec}`;
 }
